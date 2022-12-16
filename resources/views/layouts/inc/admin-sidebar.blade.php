fe189cd39ef4045dd38602697d6a696c6da390ff
manman0611 <tayobongroman@gmail.com>
2022-12-16 21:10:58 +0800

    update

--- resources/views/layouts/inc/admin-sidebar.blade.php
+++ resources/views/layouts/inc/admin-sidebar.blade.php
@@ -43,34 +43,26 @@
                     <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
                         <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
                             data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
-                            Authentication
+                            Add Invoice
                             <div class="sb-sidenav-collapse-arrow">
-                                <i class="fas fa-angle-down"></i>
                             </div>
                         </a>
-                        <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne"
-                            data-bs-parent="#sidenavAccordionPages">
-                            <nav class="sb-sidenav-menu-nested nav">
-                                <a class="nav-link" href="login.html">Login</a>
-                                <a class="nav-link" href="register.html">Register</a>
-                                <a class="nav-link" href="password.html">Forgot Password</a>
-                            </nav>
-                        </div>
                         <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
                             data-bs-target="#pagesCollapseError" aria-expanded="false"
                             aria-controls="pagesCollapseError">
-                            Error
+                            Current Invoices
                             <div class="sb-sidenav-collapse-arrow">
-                                <i class="fas fa-angle-down"></i>
+                            </div>
+                        </a>
+                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
+                            data-bs-target="#pagesCollapseError" aria-expanded="false"
+                            aria-controls="pagesCollapseError">
+                            Inactive Invoices
+                            <div class="sb-sidenav-collapse-arrow">
                             </div>
                         </a>
                         <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne"
                             data-bs-parent="#sidenavAccordionPages">
-                            <nav class="sb-sidenav-menu-nested nav">
-                                <a class="nav-link" href="401.html">401 Page</a>
-                                <a class="nav-link" href="404.html">404 Page</a>
-                                <a class="nav-link" href="500.html">500 Page</a>
-                            </nav>
                         </div>
                     </nav>
                 </div>
