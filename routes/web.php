fc848b86236ebadb97d72b2e498ce2c46149f7f2
manman0611 <tayobongroman@gmail.com>
2022-12-20 23:37:27 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\ProfileController;
+use App\Http\Controllers\Admin\InvoiceController;
+use Illuminate\Support\Facades\Auth;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -33,8 +36,15 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::prefix('admin')->group(function () {
     Route::resource('dashboard', DashboardController::class);
     Route::resource('profile', ProfileController::class);
-    Route::get('current',[ProfileController::class, 'current']);
-    Route::get('inactive',[ProfileController::class, 'inactive']);
-    Route::get('viewProfile',[ProfileController::class, 'viewProfile']);
-    Route::get('editProfile',[ProfileController::class, 'editProfile']);
+    Route::get('current', [ProfileController::class, 'current']);
+    Route::get('inactive', [ProfileController::class, 'inactive']);
+    Route::get('viewProfile', [ProfileController::class, 'viewProfile']);
+    Route::get('editProfile', [ProfileController::class, 'editProfile']);
+});
+
+Route::prefix('invoice')->group(function () {
+    Route::resource('invoice', InvoiceController::class);
+    Route::get('add_invoice', [InvoiceController::class, 'add_invoice']);
+    Route::get('current_invoice', [InvoiceController::class, 'current_invoice']);
+    Route::get('inactive_invoice', [InvoiceController::class, 'inactive_invoice']);
 });
