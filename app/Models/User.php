bc03bfc374962e910a7fb4cef84a95ab498971d7
KentYugtan <kent.yugtan95@gmail.com>
2023-03-10 22:46:32 +0800

    fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,48 +10,48 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+  use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'first_name',
-        'last_name',
-        'email',
-        'username',
-        'password',
-        'role',
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [
+    'first_name',
+    'last_name',
+    'email',
+    'username',
+    'password',
+    'role',
+  ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var array<int, string>
+   */
+  protected $hidden = [
+    'password',
+    'remember_token',
+  ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array<string, string>
+   */
+  protected $casts = [
+    'email_verified_at' => 'datetime',
+  ];
 
-    public function profile()
-    {
-        return $this->hasOne('App\Models\Profile', 'user_id');
-    }
+  public function profile()
+  {
+    return $this->hasOne('App\Models\Profile', 'user_id');
+  }
 
-    public function scopeProfile($q)
-    {
-        return $q->leftJoin('profiles', 'users.id', '=', 'profiles.user_id');
-    }
+  public function scopeProfile($q)
+  {
+    return $q->leftJoin('profiles', 'users.id', '=', 'profiles.user_id');
+  }
 }
